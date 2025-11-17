d5a37421ca4cb7668bb9b71d90efeee7c4f957f4
Yuriy <yuriy@MacBookPro.lan>
2025-11-17 23:09:15 +0300

    доработал содержание

--- contents.js
+++ contents.js
@@ -1,42 +1,46 @@
-console.log('скрипт в работе')
+console.log("скрипт в работе");
 
-const hTwo = document.querySelectorAll('h2, h3, h4')
-const contents = document.querySelector('#contents');
-const openModalBtn = document.querySelector('#openModal'); // Кнопка открытия модального окна
-const toc = document.getElementById('modal');
+const hTwo = document.querySelectorAll("h2, h3, h4");
+const contents = document.querySelector("#contents");
+const openModalBtn = document.querySelector("#openModal"); // Кнопка открытия модального окна
+const toc = document.getElementById("modal");
+let modalDisplay = toc.style.display;
 
-let arr = []
+let arr = [];
 
+// ФУНКЦИЯ ПЕРЕКЛЮЧЕНИЯ ВИДИМОСТИ МОДАЛЬНОГО ОКНА СОДЕРЖАНИЯ
+const compareStyleDisplay = () => {
+  if (document.getElementById("modal").style.display != "block") {
+    toc.style.display = "block";
+  } else {
+    toc.style.display = "none";
+  }
+};
 
-openModalBtn.addEventListener('click', () => {
-    toc.style.display = 'block';
-})
-
-//формируем массив 
-hTwo.forEach((e, index) => {
-    e.setAttribute('id', index)
-
-})
-
-const headings = document.querySelectorAll('h2, h3, h4');
-console.log(headings);
-
-headings.forEach((heading) => {
-    const link = document.createElement('a');
-    link.textContent = heading.textContent;
-    link.setAttribute('href', `#${heading.id}`);
-
-    const listItem = document.createElement('li');
-    listItem.appendChild(link);
-
-    toc.appendChild(listItem);
+openModalBtn.addEventListener("click", () => {
+  compareStyleDisplay();
 });
 
-// toc.style.display = 'none'
+//формируем массив
+hTwo.forEach((e, index) => {
+  e.setAttribute("id", index);
+});
 
-const btn1 = document.querySelector('.close')
-console.dir(toc)
-btn1.addEventListener('click', (e) => {
-    toc.style.display = 'none';
+const headings = document.querySelectorAll("h2, h3, h4");
 
-})
+headings.forEach((heading) => {
+  const link = document.createElement("a");
+  link.textContent = heading.textContent;
+  link.setAttribute("href", `#${heading.id}`);
+
+  const listItem = document.createElement("li");
+  listItem.appendChild(link);
+
+  toc.appendChild(listItem);
+});
+
+toc.querySelectorAll("li").forEach((elem) => {
+  elem.addEventListener("click", () => {
+    compareStyleDisplay();
+  });
+});
